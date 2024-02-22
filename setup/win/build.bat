d772d6d39c5fa49f927419e6b976efcbdde25f7a
LeLenoch <81lennoch@gmail.com>
2024-02-22 22:46:42 +0100

    Add: setup script for Windows

---
+++ setup/win/build.bat
@@ -0,0 +1,28 @@
+@echo off
+echo Starting...
+
+cd ..
+cd ..
+
+rem Create a virtual environment
+python -m venv JigglyConnect
+
+rem Activate the virtual environment
+call JigglyConnect\Scripts\activate.bat
+
+rem Install dependencies
+pip install -r setup/win/requirements.txt
+
+pip install --upgrade --pre --extra-index-url https://marcelotduarte.github.io/packages/ cx_Freeze
+
+rem Compile the project
+python setup/win/setup.py build
+
+rem Navigate to the build directory
+cd build
+cd exe.win-amd64-3.11
+
+rem Sign the executable
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /a /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 JigglyConnect.exe
+
+echo Build done!
---
+++ setup/win/setup.py
@@ -0,0 +1,96 @@
+from cx_Freeze import setup, Executable
+
+with open("setup/win/requirements.txt", "r") as file:
+    requirements_list = [
+        line.strip().replace("pywebview", "webview").replace("pywin32", "win32gui")
+        for line in file.readlines()
+        if line.strip() and not line.strip() == "cx_Freeze"
+    ]
+
+build_options = {
+    "include_files": [],
+    "packages": requirements_list,
+    "excludes": ["cx_Freeze"],
+    "zip_include_packages": [],
+    "zip_exclude_packages": "*"
+}
+
+import sys, os, json, shutil
+import time
+
+
+start_time = time.time()
+sys.setrecursionlimit(10000)
+base = "Win32GUI" if sys.platform == "win32" else None
+# base = 'console' if sys.platform=='win32' else None
+
+executables = [
+    Executable("main.py", base=base, target_name="JigglyConnect", icon="icon.ico")
+]
+
+with open("config.json", encoding="utf-8") as f:
+    version = json.load(f)["client-version"]
+
+setup(
+    name="JigglyConnect",
+    description="JigglyConnect",
+    author="Bishoko Team",
+    version=version,
+    options={"build_exe": build_options},
+    executables=executables,
+)
+
+
+exclude_folders = [
+    ".git",
+    ".github",
+    ".vscode",
+    "__pycache__",
+    "build",
+    "requirements.txt",
+    "build.bat",
+    "JigglyConnect.exe",
+    "icon.ico",
+    "DOC.txt",
+    "TODO.txt",
+    "debug.log",
+    "server",
+    "setup",
+    "JigglyConnect",
+]
+
+script_dir = os.getcwd()
+build_dir = os.path.join(script_dir, "build")
+
+print(script_dir)
+
+if os.path.exists(build_dir):
+    first_folder = next(
+        entry
+        for entry in os.listdir(build_dir)
+        if os.path.isdir(os.path.join(build_dir, entry))
+    )
+    target_dir = os.path.join(build_dir, first_folder)
+
+    shutil.copy2("join_room.py", os.path.join(target_dir, "lib"))
+
+    for item in os.listdir(script_dir):
+        item_path = os.path.join(script_dir, item)
+        if (
+            item != target_dir
+            and item not in exclude_folders
+            and item.endswith(".py") == False
+            and item.endswith(".md") == False
+        ):
+            if os.path.isdir(item_path):
+                shutil.copytree(item_path, os.path.join(target_dir, item))
+            else:
+                shutil.copy2(item_path, target_dir)
+            print(f"copying {item_path} -> {target_dir}")
+
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+minutes = int(elapsed_time // 60)
+seconds = int(elapsed_time % 60)
+print(f"build done! {minutes}m{seconds}s")
